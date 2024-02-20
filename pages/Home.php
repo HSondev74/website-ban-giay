e0a8398d07bafeabf46d520bae529cee5964782d
NDHIEU19092003 <nguyenduchieu148652@gmail.com>
2024-02-20 09:56:01 +0700

    best-seliing-done

--- pages/Home.php
+++ pages/Home.php
@@ -94,39 +94,61 @@
           </div>
      </div>
 
-     <div class="product-best-selling">
-          <h1 class="title-hot">Sản Phẩm Hot</h1>
-          <div class="best-selling-products">
-               <?php
-               $sql = "SELECT * FROM sanpham LIMIT 15";
-               $result = mysqli_query($conn, $sql);
-
-               if ($result->num_rows > 0) {
-                    while ($product = $result->fetch_assoc()) {
-                         $images = explode(',', $product['hinhanh']);
-                         if (!empty($images)) {
-                              $first_image = $images[0];
-                              $second_image = isset($images[1]) ? $images[1] : $images[0];
-                         }
-               ?>
-               <div class="product"
-                    onmouseover="changeImage('<?php echo $second_image; ?>', '<?php echo $product['sanpham_id']; ?>')"
-                    onmouseout="changeImage('<?php echo $first_image; ?>', '<?php echo $product['sanpham_id']; ?>')"
-                    data-id="<?php echo $product['sanpham_id']; ?>">
+     <div class="best-selling">
+          <div class="title-selling">
+               <h1>Sản Phẩm Hot</h1>
+          </div>
+          <div class="content-products">
+               <div class="product">
                     <div class="discount"> -20% </div>
                     <div class="product-image">
-                         <img src="<?php echo $first_image; ?>" alt="">
+                         <img src="https://bizweb.dktcdn.net/thumb/large/100/453/330/products/footart-anh-san-pham-website-2024-1650-px-1-1697828505660.jpg?v=1697828511123" alt="">
                          <span class="cart-popup"><i class='bx bx-cart-add'></i></span>
                     </div>
-                    <span class="heart-product" onclick="changeFavorites(this,<?php echo $product['sanpham_id']; ?>)"
-                         data-id="<?php echo $product['sanpham_id']; ?> "><i class='bx bxs-heart'></i></span>
-                    <p class=" product-title"><?php echo $product['tensanpham'] ?></p>
-                    <p class="product-price"><?php echo number_format($product['gia'], 0, ',', '.') . ' VNĐ'; ?>
-                    </p>
+                    <span class="heart-product"><i class='bx bxs-heart'></i></span>
+                    <p class=" product-title">GIÀY ADIDAS ORIGINALS SAMBA OG CLOUD WHITE CORE BLACK - B75806</p>
+                    <p class="product-price">3.400.000₫</p>
                </div>
-               <?php
-                    }
-               } ?>
+               <div class="product">
+                    <div class="discount"> -20% </div>
+                    <div class="product-image">
+                         <img src="https://bizweb.dktcdn.net/thumb/large/100/453/330/products/1-1697340489245.jpg?v=1697340499087" alt="">
+                         <span class="cart-popup"><i class='bx bx-cart-add'></i></span>
+                    </div>
+                    <span class="heart-product"><i class='bx bxs-heart'></i></span>
+                    <p class=" product-title">GIÀY ADIDAS ORIGINALS SAMBA OG CLOUD WHITE CORE BLACK - B75806</p>
+                    <p class="product-price">3.400.000₫</p>
+               </div>
+               <div class="product">
+                    <div class="discount"> -20% </div>
+                    <div class="product-image">
+                         <img src="https://bizweb.dktcdn.net/thumb/large/100/453/330/products/1-1697337325385.jpg?v=1697337337993" alt="">
+                         <span class="cart-popup"><i class='bx bx-cart-add'></i></span>
+                    </div>
+                    <span class="heart-product"><i class='bx bxs-heart'></i></span>
+                    <p class=" product-title">GIÀY ADIDAS ORIGINALS SAMBA OG CLOUD WHITE CORE BLACK - B75806</p>
+                    <p class="product-price">3.400.000₫</p>
+               </div>
+               <div class="product">
+                    <div class="discount"> -20% </div>
+                    <div class="product-image">
+                         <img src="https://bizweb.dktcdn.net/thumb/large/100/453/330/products/1-1697339753947.jpg?v=1697339760973" alt="">
+                         <span class="cart-popup"><i class='bx bx-cart-add'></i></span>
+                    </div>
+                    <span class="heart-product"><i class='bx bxs-heart'></i></span>
+                    <p class=" product-title">GIÀY ADIDAS ORIGINALS SAMBA OG CLOUD WHITE CORE BLACK - B75806</p>
+                    <p class="product-price">3.400.000₫</p>
+               </div>
+               <div class="product">
+                    <div class="discount"> -20% </div>
+                    <div class="product-image">
+                         <img src="https://bizweb.dktcdn.net/thumb/large/100/453/330/products/1-1697340659338.jpg?v=1700674285940" alt="">
+                         <span class="cart-popup"><i class='bx bx-cart-add'></i></span>
+                    </div>
+                    <span class="heart-product"><i class='bx bxs-heart'></i></span>
+                    <p class=" product-title">GIÀY ADIDAS ORIGINALS SAMBA OG CLOUD WHITE CORE BLACK - B75806</p>
+                    <p class="product-price">3.400.000₫</p>
+               </div>
           </div>
      </div>
 
