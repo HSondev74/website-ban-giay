020ee9419514efaa125228c19c785040e2c8ebff
NDHIEU19092003 <nguyenduchieu148652@gmail.com>
2024-02-21 16:59:09 +0700

    detail

---
+++ pages/Detail.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../show/css.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  </head>
+  <body>
+    <div class="container-detail">
+      <div class="detail">
+        <div class="top-title">
+          <ul class="nav-detail">
+            <li><a class="home-detail" href="">Trang chủ</a></li>
+            <li><a class="now-detail" href="">Available Now !</a></li>
+            <li>
+              <a class="name-detail" href="">
+                Giày Nike Air Force 1 Low Valentine's Day 2024 - FZ5068-161</a
+              >
+            </li>
+          </ul>
+        </div>
+
+        <div class="all-detail">
+          <div class="slide-img-detail">
+            <div class="gallery">
+              <div class="gallery-inner">
+                <img src="" alt="" />
+              </div>
+
+              <!-- <div class="control prev">
+                <i class="fa-solid fa-arrow-left"></i>
+              </div>
+              <div class="control next">
+                <i class="fa-solid fa-arrow-right"></i>
+              </div> -->
+            </div>
+
+            <div class="list">
+              <div>
+                <img src="./img-products/1-1704299109565.webp" alt="" />
+              </div>
+              <div>
+                <img src="./img-products/1-1704299483040.webp" alt="" />
+              </div>
+              <div>
+                <img src="./img-products/1-1704300361281.webp" alt="" />
+              </div>
+              <div>
+                <img src="./img-products/1-1704301799207.webp" alt="" />
+              </div>
+              <div>
+                <img src="./img-products/1-1704523456737.webp" alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div class="detail-product">
+            <div class="detail-brand">Nike</div>
+
+            <h4 class="detail-name">
+              Giày Nike Air Force 1 Low 'Year of the Dragon' 2024 - FZ5741-191
+            </h4>
+
+            <div class="detail-price">4.200.000₫</div>
+
+            <ul class="size">
+              <li>35.5</li>
+              <li>36</li>
+              <li>36.5</li>
+              <li>37.5</li>
+              <li>38</li>
+              <li>38.5</li>
+              <li>39</li>
+              <li>40</li>
+              <li>40.5</li>
+              <li>41</li>
+              <li>42</li>
+            </ul>
+
+            <div class="parameter-detail">
+                <div class="para">Code: <b>FZ5741-191</b></div>
+                <div class="para">Tình trạng: <b>Tạm hết hàng, vẫn có thể đặt hàng</b></div>
+                <div class="para">Hãng sản xuất: <b>NIKE</b></div>
+                <div class="para">Xuất xứ thương hiệu: <b>Hàng xách tay</b></div>
+                <div class="para">Chủng loại: <b>Giày</b></div>
+            </div>
+
+            <div class="detail-ship">
+              <div class="detail-icon">
+                  <img src="./img-icon/icon_service_product_1.svg" alt="">
+                  <p>Giao hàng toàn quốc (Hỗ trợ ship COD nhận hàng thanh toán)<p>
+              </div> 
+              <div class="detail-icon">
+                  <img src="./img-icon/icon_service_product_2.svg" alt="">
+                  <p>Nhận ngay QUÀ TẶNG và VOUCHER giảm giá cho lần mua hàng tiếp theo<p>
+              </div> 
+              <div class="detail-icon">
+                  <img src="./img-icon/icon_service_product_3.svg" alt="">
+                  <p>
+                    Hỗ trợ đổi size và đổi mẫu trong vòng 5 ngày<p>
+              </div> 
+              <div class="detail-icon">
+                  <img src="./img-icon/icon_service_product_4.svg" alt="">
+                  <p>Cam kết hàng chính hãng 100%<p>
+              </div> 
+            </div>
+
+            <div class="detail-like">
+              <i class="fa-regular fa-heart"></i>
+              <p>Yêu thích</p>
+            </div>
+
+            <!-- <div class="up-and-downproduct">
+              <a href="pages/addProduct.php?decrease=<?php echo $item['id']; ?>" class="quantity-decrease" data-id="<?php echo $item['id']; ?>">-</a>
+              <span class="quantity-prd"><?php echo $item['soluong']; ?></span>
+              <a href="pages/addProduct.php?increase=<?php echo $item['id']; ?>" class="quantity-increase" data-id="<?php echo $item['id']; ?>">+</a>
+            </div> -->
+
+            <div class="detail-pay">
+              <div class="detail-add-pay">Thêm vào giỏ hàng</div>
+              <div class="detail-buy">Mua Ngay</div>
+            </div>
+
+            <div class="detail-contact">
+              <div class="zalo">Liên hệ Zalo</div>
+              <div class="faebook">Liên hệ Fanpage Facebook</div>
+              <div class="insta">Liên hệ Instagam</div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+  <script src="./js.js"></script>
+</html>
