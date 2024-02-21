69c46cdd901b045edfde84d749dbac06fe6f89a7
NDHIEU19092003 <nguyenduchieu148652@gmail.com>
2024-02-21 08:11:21 +0700

    test

--- JS/slideShowDetail.js
+++ JS/slideShowDetail.js
@@ -1,8 +1,9 @@
 var list = document.querySelectorAll('.list img')
-var gallery = document.querySelector('.gallery')
-var galleryInner = document.querySelector('.gallery-inner img')
-var prev = document.querySelector('.control.prev')
-var next = document.querySelector('.control.next')
+console.log(list);
+// var gallery = document.querySelector('.gallery')
+// var galleryInner = document.querySelector('.gallery-inner img')
+// var prev = document.querySelector('.control.prev')
+// var next = document.querySelector('.control.next')
 
 var curenIndex = 0
 
@@ -21,34 +22,34 @@ list.forEach((item,index)=>{
     })
 })
 
-next.addEventListener('click', ()=>{
-    curenIndex++
-    if(curenIndex >= list.length )
-    {
-        curenIndex = 0
-        var a = list[curenIndex].src
-        galleryInner.setAttribute('src', `${a}`)
-    }
-    else
-    {
-        var a = list[curenIndex].src
-        galleryInner.setAttribute('src', `${a}`)
-    }
+// next.addEventListener('click', ()=>{
+//     curenIndex++
+//     if(curenIndex >= list.length )
+//     {
+//         curenIndex = 0
+//         var a = list[curenIndex].src
+//         galleryInner.setAttribute('src', `${a}`)
+//     }
+//     else
+//     {
+//         var a = list[curenIndex].src
+//         galleryInner.setAttribute('src', `${a}`)
+//     }
 
-})
+// })
 
-prev.addEventListener('click', ()=>{
-    curenIndex--
-    if(curenIndex <= 0 )
-    {
-        curenIndex = list.length -1 
-        var a = list[curenIndex].src
-        galleryInner.setAttribute('src', `${a}`)
-    }
-    else
-    {
-        var a = list[curenIndex].src
-        galleryInner.setAttribute('src', `${a}`)
-    }
+// prev.addEventListener('click', ()=>{
+//     curenIndex--
+//     if(curenIndex <= 0 )
+//     {
+//         curenIndex = list.length -1 
+//         var a = list[curenIndex].src
+//         galleryInner.setAttribute('src', `${a}`)
+//     }
+//     else
+//     {
+//         var a = list[curenIndex].src
+//         galleryInner.setAttribute('src', `${a}`)
+//     }
 
-})
+// })
