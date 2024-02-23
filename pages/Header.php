432f1f64a2049f0d1e817ac6e6267823b9ac267b
HSonDev74 <lesonkyac@gmail.com>
2024-02-23 07:57:47 +0700

    admin: fix

--- pages/Header.php
+++ pages/Header.php
@@ -28,7 +28,10 @@
                     <div class="close-map">x</div>
                </div>
                <div class="img-map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.70066492663!2d105.7470029747149!3d21.04465988723339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454edcdbc166f%3A0xa9f96e0cf23b6dc4!2zTmcuIDE3NyDEkC4gQ-G6p3UgRGnhu4VuLCBLaeG7gXUgTWFpLCBQaMO6YyBEaeG7hW4sIELhuq9jIFThu6sgTGnDqm0sIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1708398620880!5m2!1svi!2s" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe
+                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.70066492663!2d105.7470029747149!3d21.04465988723339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454edcdbc166f%3A0xa9f96e0cf23b6dc4!2zTmcuIDE3NyDEkC4gQ-G6p3UgRGnhu4VuLCBLaeG7gXUgTWFpLCBQaMO6YyBEaeG7hW4sIELhuq9jIFThu6sgTGnDqm0sIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1708398620880!5m2!1svi!2s"
+                         width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                         referrerpolicy="no-referrer-when-downgrade"></iframe>
                </div>
           </div>
      </div>
@@ -43,7 +46,8 @@
                     </div>
                </a>
                <div class="header-search__input-section">
-                    <form role="search" autocomplete="off" action="index.php?action=search" method="POST" class="umine-searchbar">
+                    <form role="search" autocomplete="off" action="index.php?action=search" method="POST"
+                         class="umine-searchbar">
                          <div class="umine-select-categories">
                               <select name="Categories">
                                    <option value="" selected>Toàn Bộ Hãng</option>
@@ -57,8 +61,8 @@
                                         // Duyệt qua từng danh mục và tạo các tùy chọn cho trường select
                                         while ($category = mysqli_fetch_assoc($result_categories)) {
                                    ?>
-                                             <option value="<?php echo $category['danhmuc_id']; ?>">
-                                                  <?php echo $category['tendanhmuc']; ?></option>
+                                   <option value="<?php echo $category['danhmuc_id']; ?>">
+                                        <?php echo $category['tendanhmuc']; ?></option>
                                    <?php
                                         }
                                    }
@@ -67,9 +71,12 @@
                          </div>
                          <div class="h-line-searchbar"></div>
                          <div class="umine-searchbar__main">
-                              <div class="umine-searchbar-input"><input class="umine-searchbar-input__input" maxlength="128" placeholder="Tìm kiếm hơn 200+ sản phẩm..." autocomplete="off" aria-expanded="false" role="combobox" value="" name="keyword">
+                              <div class="umine-searchbar-input"><input class="umine-searchbar-input__input"
+                                        maxlength="128" placeholder="Tìm kiếm hơn 200+ sản phẩm..." autocomplete="off"
+                                        aria-expanded="false" role="combobox" value="" name="keyword">
                               </div>
-                         </div><button type="submit" name="searchHeader" class="umine-searchbar-button" style="cursor: pointer;">
+                         </div><button type="submit" name="searchHeader" class="umine-searchbar-button"
+                              style="cursor: pointer;">
                               Tìm Kiếm
                          </button>
                     </form>
@@ -77,24 +84,25 @@
                <div class="header-search-cart flex v-center">
                     <div class="header-search-cart-account flex">
                          <?php if (isset($_SESSION['dangnhap']) && !empty($_SESSION['dangnhap'])) : ?>
-                              <div class="account-icon" style="display: flex;">
-                                   <a class="info-login" href="/bangiay/pages/logout.php">
-                                        <i class='bx bx-user'></i>
-                                        <p>Đăng xuất</p>
-                                   </a>
-                                   <nav>
-                                        <p><?php echo $_SESSION['dangnhap']['ten']; ?></p>
-                                        <p><?php echo $_SESSION['dangnhap']['email']; ?></p>
-                                   </nav>
-                              </div>
+                         <div class="account-icon" style="display: flex;">
+                              <nav>
+                                   <p><i class='bx bx-user'></i></p>
+                              </nav>
+                              <a class="info-login" style="display: grid; justify-content: center; "
+                                   href="/bangiay/pages/logout.php">
+                                   <p><?php echo $_SESSION['dangnhap']['ten']; ?></p>
+                                   <p style="color: #000; font-weight: 700;">Đăng xuất</p>
+                              </a>
+
+                         </div>
                          <?php else : ?>
-                              <div class="account-icon">
-                                   <a href="index.php?action=login"><i class='bx bx-user'></i></a>
-                              </div>
-                              <div class="account-title">
-                                   <a class="account-title-login" href="index.php?action=login">Đăng nhập</a>
-                                   <p class="account-title-acc">Tài khoản</p>
-                              </div>
+                         <div class="account-icon">
+                              <a href="index.php?action=login"><i class='bx bx-user'></i></a>
+                         </div>
+                         <div class="account-title">
+                              <a class="account-title-login" href="index.php?action=login">Đăng nhập</a>
+                              <p class="account-title-acc">Tài khoản</p>
+                         </div>
                          <?php endif; ?>
                     </div>
                     <div class="header-favorites">
@@ -112,18 +120,18 @@
                                    $totalItems = count($_SESSION['cart']);
                                    // Tính tổng số lượng các mục trong session
                          ?>
-                                   <div class="carts">
-                                        <span class="cart"><?php echo $totalItems; ?></span>
-                                        <i class='bx bx-cart'></i>
-                                   </div>
-                              <?php
+                         <div class="carts">
+                              <span class="cart"><?php echo $totalItems; ?></span>
+                              <i class='bx bx-cart'></i>
+                         </div>
+                         <?php
                               } else {
                               ?>
-                                   <div class="carts">
-                                        <span class="cart">0</span>
-                                        <i class='bx bx-cart'></i>
-                                   </div>
-                              <?php
+                         <div class="carts">
+                              <span class="cart">0</span>
+                              <i class='bx bx-cart'></i>
+                         </div>
+                         <?php
                               }
 
                               $totalPrice = 0;
@@ -131,21 +139,21 @@
                                    $totalPrice += $item['gia'] * $item['soluong'];
                               }
                               ?>
-                              <div class="cart-price">
-                                   <p>Your Cart</p>
-                                   <strong><?php echo number_format($totalPrice) . " VNĐ" ?></strong>
-                              </div>
+                         <div class="cart-price">
+                              <p>Your Cart</p>
+                              <strong><?php echo number_format($totalPrice) . " VNĐ" ?></strong>
+                         </div>
                          <?php
                          } else {
                          ?>
-                              <div class="carts">
-                                   <span class="cart">0</span>
-                                   <i class='bx bx-cart'></i>
-                              </div>
-                              <div class="cart-price">
-                                   <p>Your Cart</p>
-                                   <strong style="margin-left: 15px;">0 VNĐ</strong>
-                              </div>
+                         <div class="carts">
+                              <span class="cart">0</span>
+                              <i class='bx bx-cart'></i>
+                         </div>
+                         <div class="cart-price">
+                              <p>Your Cart</p>
+                              <strong style="margin-left: 15px;">0 VNĐ</strong>
+                         </div>
                          <?php
                          }
                          ?>
